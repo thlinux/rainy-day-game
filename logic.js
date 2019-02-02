8657975aa1c75f14038d6a38c4f14ee8738a81de
Tristan Hull <soldier@kenpokicks.com>
2019-02-02 13:06:44 -0800

    testing drawing on canvas

--- logic.js
+++ logic.js
@@ -3,3 +3,10 @@ let canvas = document.getElementById("myCanvas");
 
 //storing the 2d rendering context
 let ctx = canvas.getContext("2d");
+
+//testing printing of canvas
+ctx.beginPath();
+ctx.rect(20, 40, 50, 50);
+ctx.fillStyle =  "#FF0000";
+ctx.fill();
+ctx.closePath();
