6ebc1a50ec28e3ad0e2d73b4160b35e6f6f60375
Tristan Hull <soldier@kenpokicks.com>
2019-02-02 13:12:40 -0800

    testing movement

--- logic.js
+++ logic.js
@@ -4,21 +4,33 @@ let canvas = document.getElementById("myCanvas");
 //storing the 2d rendering context
 let ctx = canvas.getContext("2d");
 
-//testing printing of canvas
-ctx.beginPath();
-ctx.rect(20, 40, 50, 50);
-ctx.fillStyle =  "#FF0000";
-ctx.fill();
-ctx.closePath();
+// //testing printing of canvas
+// ctx.beginPath();
+// ctx.rect(20, 40, 50, 50);
+// ctx.fillStyle =  "#FF0000";
+// ctx.fill();
+// ctx.closePath();
 
-ctx.beginPath();
-ctx.arc(240, 160, 20, 0, Math.PI*2, false);
-ctx.fillStyle = "green";
-ctx.fill();
-ctx.closePath();
+// ctx.beginPath();
+// ctx.arc(240, 160, 20, 0, Math.PI*2, false);
+// ctx.fillStyle = "green";
+// ctx.fill();
+// ctx.closePath();
 
-ctx.beginPath();
-ctx.rect(160, 10, 100, 40);
-ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
-ctx.stroke();
-ctx.closePath();
+// ctx.beginPath();
+// ctx.rect(160, 10, 100, 40);
+// ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
+// ctx.stroke();
+// ctx.closePath();
+
+//animation testing
+
+function draw() {
+    ctx.beginPath();
+    ctx.arc(50, 50, 10, 0, Math.PI*2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
+setInterval(draw, 10);
