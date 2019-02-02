5655809fb481b049aeed666c28e58204712f3ca1
Tristan Hull <soldier@kenpokicks.com>
2019-02-02 15:40:17 -0800

    collosion detection for bricks

--- logic.js
+++ logic.js
@@ -32,7 +32,7 @@ let bricks = [];
 for (let c = 0; c < brickColumnCount; c++) {
     bricks[c] = [];
     for(let r = 0; r < brickRowCount; r++) {
-        bricks[c][r] = { x: 0, y: 0}
+        bricks[c][r] = { x: 0, y: 0, status: 1}
     }
 
 }
@@ -63,16 +63,17 @@ function keyUpHandler(e) {
 function drawBricks() {
     for(let c = 0; c < brickColumnCount; c++) {
         for(let r = 0; r < brickRowCount; r++) {
-            let brickX = (c * (brickWidth + brickPadding)) + brickOffsetLeft;
-            let brickY = (r * (brickHeight+ brickPadding)) + brickOffsetTop;
-            bricks[c][r].x = brickX;
-            bricks[c][r].y = brickY;
-            ctx.beginPath();
-            ctx.rect(brickX, brickY, brickWidth, brickHeight);
-            ctx.fillStyle = "#0095DD";
-            ctx.fill();
-            ctx.closePath();
-
+            if(bricks[c][r].status == 1) {
+                var brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
+                var brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
+                bricks[c][r].x = brickX;
+                bricks[c][r].y = brickY;
+                ctx.beginPath();
+                ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                ctx.fillStyle = "#0095DD";
+                ctx.fill();
+                ctx.closePath();
+            }
         }
     }
 }
@@ -94,6 +95,20 @@ function drawPaddle() {
     ctx.closePath();
 }
 
+function collosionDetection() {
+    for(let c = 0; c < brickColumnCount; c++) {
+        for(let r = 0; r < brickRowCount; r++) {
+            let b = bricks[c][r];
+            if(b.status == 1) {
+                if(x > b.x && x < b.x+brickWidth && y > b.y && y < b.y+brickHeight) {
+                    dy = -dy;
+                    b.status = 0;
+                }
+            }   
+        }
+    }
+}
+
 function draw() {
     //clearing canvas 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -103,7 +118,8 @@ function draw() {
     drawBall();
     //calling draw paddle
     drawPaddle();
-
+    //calling collesion detection
+    collosionDetection();
     //changing the x and y value of the ball
     x += dx;
     y += dy;
